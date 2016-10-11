36d974890c6cb83620d7f6541395a1bbfc6f47f0
fbell123 <fbell.pro@gmail.com>
2016-10-11 11:53:15 +0100

    added in capybara and selenium-webdriver

--- Gemfile
+++ Gemfile
@@ -5,3 +5,10 @@ ruby "2.3.0"
 gem 'sinatra'
 gem 'rspec'
 gem 'shotgun'
+gem 'capybara'
+gem 'selenium-webdriver'
+
+group :development, :test do
+  gem 'pry'
+  gem 'rb-readline'
+end
--- app.rb
+++ app.rb
@@ -3,16 +3,21 @@ require 'shotgun'
 
 set :session_secret, 'super secret'
 
+get '/' do
+  'Hello world!'
+end
 
 get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
 end
 
-get '/form'
+get '/cat-form' do
+  erb(:cat_form)
+end
