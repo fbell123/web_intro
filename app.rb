ddc6c4a7b1395a65006035471027c3291ffda88c
Ed Powderham <edpowderham@outlook.com>
2016-10-11 11:55:15 +0100

    updated index

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 
+
+
 get '/' do
   "Hello World"
 end
@@ -16,9 +18,14 @@ get '/run' do
   "I should really go for a run"
 end
 
-get '/cat' do
+get '/random-cat' do
+@name = ["Amigo", "Oscar", "Viking"].sample
 erb(:index)
 end
 
-
+get '/named-cat' do
+  p params
+@name = params[:name]
+erb(:index)
+end
 # <img src="smiley.gif" alt="Smiley face" border="5">
