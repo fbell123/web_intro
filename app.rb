72ccaa536259324e6b7d703640106c0aaea137b9
Anna Beddow <anna.beddow@gmail.com>
2016-10-11 12:04:00 +0100

    added POST method

--- app.rb
+++ app.rb
@@ -23,9 +23,13 @@ get '/random-cat' do
 erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
 @name = params[:name]
 erb(:index)
+end
+
+get '/form' do
+  erb (:form)
 end
 # <img src="smiley.gif" alt="Smiley face" border="5">
