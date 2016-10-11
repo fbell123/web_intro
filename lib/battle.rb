3acfa69675ec00065b7733675efab6b494707082
Anna Beddow <anna.beddow@gmail.com>
2016-10-11 16:55:46 +0100

    reverted to inital setup

--- lib/battle.rb
+++ lib/battle.rb
@@ -1,13 +1,28 @@
-require 'sinatra'
+require 'sinatra/base'
+# require 'sinatra'
+# require 'lib'
 
-get '/' do
-  erb(:index)
+class Battle < Sinatra::Base
+  #
+  # #
+  # post '/setup' do
+  #   @p1_name = params[:player1_name]
+  #   @p2_name = params[:player2_name]
+  #   @p1_points = 100
+  #   @p2_points = 100
+  #
+  #   erb(:battle)
+  # end
+
+  get '/' do
+    'Hello Battle!'
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
 
-post '/setup' do
-  @p1_name = params[:player1_name]
-  @p2_name = params[:player2_name]
-  @p1_points = 100
-  @p2_points = 100
-  erb(:battle)
-end
+#
+# get '/' do
+#   erb(:index)
+# end
