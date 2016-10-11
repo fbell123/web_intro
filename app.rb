2df4220134b13926d32ad8011560a3d36bd7167d
fbell123 <fbell.pro@gmail.com>
2016-10-11 09:53:50 +0100

    pulled day 1

--- app.rb
+++ app.rb
@@ -3,17 +3,6 @@ require 'shotgun'
 
 set :session_secret, 'super secret'
 
-get '/' do
-  'Hello world!'
-end
-
-get '/secret' do
-  'My dirty little secret is?'
-end
-
-get '/puppy' do
-  'Bow wow wow'
-end
 
 get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
@@ -26,10 +15,4 @@ get '/named-cat' do
   erb(:index)
 end
 
-get '/turtle' do
-  'Squeak wow wow'
-end
-
-get '/chinchilla' do
-  'Bark wee oui'
-end
+get '/form'
