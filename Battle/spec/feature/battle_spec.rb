2ddf054b6322e14130ff9876e12b58384bbe0b80
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-10-11 16:30:07 +0100

    added an attack button and a confirmation message

--- Battle/spec/feature/battle_spec.rb
+++ Battle/spec/feature/battle_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require '././cool_app.rb'
 #require '././index.erb'
 
-RSpec.feature "Battle management", :type => :feature do
+RSpec.feature "User input", :type => :feature do
   scenario "user fills in form with name" do
   sign_in_and_play
   end
@@ -13,5 +13,13 @@ RSpec.feature "Battle hitpoints", :type => :feature do
     visit "/play"
 
     expect(page).to have_content '100HP'
+  end
+end
+
+RSpec.feature "Battle attack", :type => :feature do
+  scenario "allows attack and gives confirmation" do
+  sign_in_and_play
+  click_button('ATTACK!')
+  expect(page).to have_content 'Tarquinn stabs Gwenevere. ouch.'
   end
 end
