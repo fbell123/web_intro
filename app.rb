88e936c511a21aecfafcf930bfb9dfde4b28b067
Anna Beddow <anna.beddow@gmail.com>
2016-10-11 16:04:51 +0100

    added rspec-sinatra

--- app.rb
+++ app.rb
@@ -1,11 +1,13 @@
 require 'sinatra'
 
-
-
 get '/' do
   erb(:index)
 end
 
 post '/setup' do
-  "Hello"
+  @p1_name = params[:player1_name]
+  @p2_name = params[:player2_name]
+  @p1_points = 100
+  @p2_points = 100
+  erb(:battle)
 end
